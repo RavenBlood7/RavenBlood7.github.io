60738bfd4cc7e13a1e73d0145cef0cc42cb83512
RavenBlood7 <juan.dupreez82@gmail.com>
2017-07-18 12:16:23 +0200

    Try some new stuffs

---
+++ DT/DT.js
@@ -0,0 +1,21 @@
+//append entry to csv file in following format:
+	//date 
+	//time
+	//overall
+	//...
+function writeToFile()
+{
+	//var file = new File("DT.dat");
+	//file.writeln("anything");
+	//file.close();
+	
+	var f = "DT.dat";
+	
+	writeTextFile(f, "hello");
+}
+	
+//verify
+function verify()
+{
+	
+}
