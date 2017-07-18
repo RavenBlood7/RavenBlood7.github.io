98b5f340f2069cf1f2e9ac9564a9b010c0e7ca04
RavenBlood7 <juan.dupreez82@gmail.com>
2017-07-18 21:14:46 +0200

    Store POSTed data in a .dat file

---
+++ DT/DT.php
@@ -0,0 +1,28 @@
+Thank you for contributing. The details have been added:
+<?php
+
+$format = "d F 'y ,h:m:s";
+$toWrite = 	date_format(date_create(), $format).',';
+$toWrite .= 	$_POST["TOD"].','.
+			$_POST["overall"].','.
+			$_POST["feelings"].','.
+			$_POST["thoughts"].','.
+			$_POST["confidence"].','.
+			$_POST["emo_energy"].','.
+			$_POST["ment_energy"].','.
+			$_POST["phy_energy"].','.
+			$_POST["popcorn"].','.
+			$_POST["circumstance"].','.
+			$_POST["pain"].','.
+			$_POST["loneliness"].','.
+			$_POST["depression"].','.
+			$_POST["diet"].','.
+			$_POST["social"].','.
+			$_POST["location"].','.
+			$_POST["input"].','.
+			$_POST["comment"]."\n";
+			
+file_put_contents("DT.dat", $toWrite, FILE_APPEND);
+echo $toWrite;
+
+?>
